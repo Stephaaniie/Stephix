4480e65bf3ea882d29e9c71f00c7b303e8a10541
stephaaniie <stephanie.castillo@gmail.com>
2020-07-27 19:00:18 -0300

    se modifico el archivo de maven

--- src/main/java/ar/com/ada/api/stephix/security/Crypto.java
+++ src/main/java/ar/com/ada/api/stephix/security/Crypto.java
@@ -70,15 +70,4 @@ public class Crypto {
         }
         return null;
     }
-
-    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
-        BigInteger bi = new BigInteger(1, array);
-        String hex = bi.toString(16);
-        int paddingLength = (array.length * 2) - hex.length();
-        if (paddingLength > 0) {
-            return String.format("%0" + paddingLength + "d", 0) + hex;
-        } else {
-            return hex;
-        }
-    }
 }
